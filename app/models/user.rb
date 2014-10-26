3256eb0027343bd85ed06c1a1551aad25e0f70dd
Masaya Konishi <konishi@office-ore.net>
2014-10-26 23:47:04 +0900

    user modelにoauth認証時のユーザー取得(or 新規作成)のメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :trackable, :omniauthable, :omniauth_providers => [:github]
-  
+
+  def self.find_for_oauth(auth)
+    u = User.where(:provider => auth["provider"], :uid => auth.uid)
+    case auth["provider"].to_sym
+      when :github
+        u.first_or_initialize(:name => auth.extra.raw_info.name, :email => auth.info.email)
+    end
+  end
 end
