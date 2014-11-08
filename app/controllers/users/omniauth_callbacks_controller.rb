e0cf0ac178eae38c431265acd52b0cf7a4516628
Masaya Konishi <konishi@office-ore.net>
2014-11-08 14:28:40 +0900

    githubでログイン可能にした

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,13 @@
+class Users::OmniauthCallbacksController < ApplicationController
+  def github
+    @user = User.find_for_oauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Github"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      redirect_to root_path
+    end
+
+  end
+end
