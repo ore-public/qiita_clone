972082b9af68488e4da8ab7e29261056896118d7
Masaya Konishi <konishi@office-ore.net>
2014-12-08 07:52:43 +0900

    公開記事の更新メソッド作成
    （テスト未通過）

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -19,4 +19,12 @@ class Draft < Article
       raise 'Already created public item.'
     end
   end
+
+  def update_public_item
+    if self.item
+      self.item.update_attributes(self.get_contents)
+    else
+      raise 'Public item not created.'
+    end
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -11,8 +11,8 @@ describe Article, type: :model do
   it "同じitem_tokenでデータが出来ても, slugが異なること" do
     draft1 = Draft.create!(title: '下書き1', item_token: 'hoge')
     draft2 = Draft.create!(title: '下書き2', item_token: 'hoge')
-    expect(draft1.slug).not_to eq(draft2.slug)
-  end
+    expect(draft1.slug).not_to eq(draft2.slug) 
+o end
 
   context '下書きから公開記事を作る（記事が未公開の場合)' do
     it '下書きから公開記事作成。下書きの内容と同じであること' do
@@ -37,7 +37,22 @@ describe Article, type: :model do
     it 'すでに公開済みの下書きで、公開メソッドを呼ぶとエラーになること' do
       draft = Draft.create!(title: '下書き', raw_body: '# タイトル')
       draft.new_public_item
-      expect(draft.new_public_item).to raise_error
+      expect{draft.new_public_item}.to raise_error
     end
+  end
+
+  context '下書きの編集結果を公開記事に反映する(記事が公開済みの場合)' do
+    it '記事を公開後、下書きの編集をして公開記事を更新出来ること' do
+      draft = Draft.create!(title: 'タイトル')
+      draft.new_public_item
+
+      draft.title = 'タイトル upd'
+      draft.raw_body = '本文 upd'
+      draft.save!
+      draft.item.update_public_item
+
+      item = draft.item
+      expect(item.title).to eq('タイトル upd')
+      expect(item.raw_body).to eq('本文 upd')
   end
 end
