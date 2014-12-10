e5b1a50e886b1b60f6a697407177c88dfcac6b4c
Masaya Konishi <konishi@office-ore.net>
2014-12-10 12:08:32 +0900

    下書きの作成のテスト作成

--- spec/controllers/drafts_controller_spec.rb
+++ spec/controllers/drafts_controller_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe DraftsController, :type => :controller do
+  let(:drafts_attributes){
+    {title: '下書き', raw_body: ''}
+  }
+
+  let(:public_create_attributes){
+    {title: '公開新規', public_crate: '投稿'}
+  }
+
+  let(:public_update_attributes){
+    {title: '公開更新', public_crate: '投稿'}
+  }
   context '記事の作成、編集、公開' do
     before do
       @user1 = User.create!(email: 'user1@mail.com')
@@ -9,6 +20,10 @@ RSpec.describe DraftsController, :type => :controller do
     
     it '下書きとの作成' do
       sign_in @user1
+
+      expect {
+        post :create, {:draft => drafts_attributes}
+      }.to change(Draft, :count).by(1)
     end
   end
 end
