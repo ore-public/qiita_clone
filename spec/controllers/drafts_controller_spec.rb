fc343ce4c30547a0c4dd1e8fac3d16fc54befb48
Masaya Konishi <konishi@office-ore.net>
2014-12-18 22:20:03 +0900

    下書きと公開記事両方の更新をテスト

--- spec/controllers/drafts_controller_spec.rb
+++ spec/controllers/drafts_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe DraftsController, :type => :controller do
-  let(:drafts_attributes){
+  let(:drafts_attributes) {
     {title: '下書き', raw_body: ''}
   }
 
@@ -10,7 +10,7 @@ RSpec.describe DraftsController, :type => :controller do
       @user1 = User.create!(email: 'user1@mail.com')
       @user2 = User.create!(email: 'user2@mail.com')
     end
-    
+
     it '下書きの作成' do
       sign_in @user1
 
@@ -19,12 +19,16 @@ RSpec.describe DraftsController, :type => :controller do
       }.to change(Draft, :count).by(1)
     end
 
-    it '下書き保存せずすぐに公開' do
-      sign_in @user1
+    context '下書き保存せずすぐに公開' do
+      before do
+        sign_in @user1
+        @result = expect do
+          post :create, {draft: {title: '公開新規'}, public_create: '投稿'}
+        end
+      end
 
-      expect {
-        post :create, {draft: {title: '公開新規'}, public_create: '投稿'}
-      }.to change(Item, :count).by(1)
+      it { @result.to change(Item, :count).by(1) }
+      it { @result.to change(Draft, :count).by(1) }
     end
 
     it '下書きを編集して公開' do
@@ -37,6 +41,5 @@ RSpec.describe DraftsController, :type => :controller do
         post :update, {id: draft.id, draft: {title: '編集して公開'}, public_create: '投稿'}
       }.to change(Item, :count).by(1)
     end
-
   end
 end
