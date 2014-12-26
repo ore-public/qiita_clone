c9edff54514735f8ab876d6362c57f8d95cf579a
Masaya Konishi <konishi@office-ore.net>
2014-12-26 18:46:24 +0900

    記事の作成ユーザーを必須にした
    closed #47

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 
 describe Article, type: :model do
 
+  before do
+    @user = User.create!(email: 'user@mail.com')
+  end
+
   it "下書き記事を作成、item_token, slugが生成されること" do
-    draft = Draft.create!(title: '下書き')
+    draft = @user.drafts.build(title: '下書き')
+    draft.save!
     expect(draft.item_token).not_to eq(nil)
     expect(draft.slug).not_to eq(nil)
   end
 
   it "同じitem_tokenでデータが出来ても, slugが異なること" do
-    draft1 = Draft.create!(title: '下書き1', item_token: 'hoge')
-    draft2 = Draft.create!(title: '下書き2', item_token: 'hoge')
+    draft1 = @user.drafts.build(title: '下書き1', item_token: 'hoge')
+    draft1.save!
+    draft2 = @user.drafts.build(title: '下書き2', item_token: 'hoge')
+    draft2.save!
     expect(draft1.slug).not_to eq(draft2.slug) 
   end
 
   context '下書きから公開記事を作る（記事が未公開の場合)' do
     it '下書きから公開記事作成。下書きの内容と同じであること' do
-      draft = Draft.create!(title: '下書き', raw_body: '# タイトル')
+      draft = @user.drafts.build(title: '下書き', raw_body: '# タイトル')
+      draft.save!
       item = draft.new_public_item
       expect(item).to be_persisted
       expect(item.title).to eq(draft.title)
@@ -26,16 +34,19 @@ describe Article, type: :model do
     end
 
     it 'item_tokenが重複した下書きがある場合、公開記事のslugが下書きと同期とれていること' do
-      draft = Draft.create!(title: '下書き', item_token: 'hoge')
+      draft = @user.drafts.build(title: '下書き', item_token: 'hoge')
+      draft.save!
       item = draft.new_public_item
-      draft2 = Draft.create!(title: '下書き2', item_token: 'hoge')
+      draft2 = @user.drafts.build(title: '下書き2', item_token: 'hoge')
+      draft2.save!
       item2 = draft2.new_public_item
       expect(item.slug).to eq(draft.slug)
       expect(item2.slug).to eq(draft2.slug)
     end
 
     it 'すでに公開済みの下書きで、公開メソッドを呼ぶとエラーになること' do
-      draft = Draft.create!(title: '下書き', raw_body: '# タイトル')
+      draft = @user.drafts.build(title: '下書き', raw_body: '# タイトル')
+      draft.save!
       draft.new_public_item
       expect{draft.new_public_item}.to raise_error
     end
@@ -43,7 +54,8 @@ describe Article, type: :model do
 
   context '下書きの編集結果を公開記事に反映する(記事が公開済みの場合)' do
     it '記事を公開後、下書きの編集をして公開記事を更新出来ること' do
-      draft = Draft.create!(title: 'タイトル')
+      draft = @user.drafts.build(title: 'タイトル')
+      draft.save!
       draft.new_public_item
 
       draft.title = 'タイトル upd'
@@ -56,7 +68,8 @@ describe Article, type: :model do
     end
 
     it '記事未公開で下書きの更新メソッド呼ぶとエラーになること' do
-      draft = Draft.create!(title: 'タイトル')
+      draft = @user.drafts.build(title: 'タイトル')
+      draft.save!
       expect{draft.update_public_item}.to raise_error
     end
   end
