bed844f1b2c874f2da1b8fc8a73372babf999ea2
Masaya Konishi <konishi@office-ore.net>
2015-01-10 16:45:00 +0900

    記事のストック後、ストックボタンの更新をjsのレスポンスで書き換えるように変更

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,17 +1,21 @@
 class StocksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: %i(create destroy)
 
   def create
-    current_user.stock_items << Item.friendly.find(params[:item_id])
-    render js: 'location.reload();'
+    current_user.stock_items << @item
   end
 
   def destroy
-    current_user.stocks.find_by_item_id!(Item.friendly.find(params[:item_id])).destroy
-    render js: 'location.reload();'
+    current_user.stocks.find_by_item_id!(@item).destroy
   end
 
   def index
     @stocks = User.friendly.find(params[:user_id]).stock_items.page(params[:page])
+  end
+
+  private
+  def set_item
+    @item = Item.friendly.find(params[:item_id])
   end
 end
