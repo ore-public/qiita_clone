9e0e397547c20b1912c1be69b8df2c543bb7b850
Masaya Konishi <konishi@office-ore.net>
2015-01-12 12:46:19 +0900

    followモデルのfollow, followerの関連が逆だったのを修正

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,4 @@
 class Follow < ActiveRecord::Base
-  belongs_to :follower_user, class_name: 'User', foreign_key: :user_id
-  belongs_to :follow_user, class_name: 'User', foreign_key: :follower_id
+  belongs_to :follower_user, class_name: 'User', foreign_key: :follower_id
+  belongs_to :follow_user, class_name: 'User', foreign_key: :user_id
 end
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Follow, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'フォローする' do
+    before do
+      @follow_user = FactoryGirl.create(:user1)
+      @followed_user1 = FactoryGirl.create(:user2)
+      @followed_user2 = FactoryGirl.create(:user3)
+
+      follow1 = @followed_user1.follows.build(follower_id: @follow_user.id)
+      follow1.save!
+
+      follow2 = @followed_user2.follows.build(follower_id: @follow_user.id)
+      follow2.save!
+    end
+
+    it 'フォロー済みユーザーの一覧が取得出来ること' do
+      expect(@followed_user1.followed?(@follow_user)).to be_truthy
+      expect(@followed_user2.followed?(@follow_user)).to be_truthy
+    end
+  end
 end
